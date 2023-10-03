4aef48b197b4508a0a41507f42d75a881233072c
Dan <sandersda@gmail.com>
2023-10-03 08:50:02 -0500

    Added psuedo code and HTML Layout

---
+++ index.js
@@ -0,0 +1,4 @@
+// create object with names, dates, times, locations, and description of parties
+// insert a list item for each party that displays in the partyList section of html
+// create a delete button for each party
+// use event listener to fetch data for new party and return it to the partyList object
